77cc331aee69e6e17baf2440f36d267e33545bf9
PrashikBhimte <prashikbhimte29@gmail.com>
2024-11-23 16:38:58 +0530

    writting animation complete.

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,68 +8,72 @@ import linkedin from "../Images/linkedin.png";
 
 export default function Home() {
 
-  // const [text, setText] = useState('');
-  // const [isWriting, setIsWriting] = useState(true);
-  // const textRef = useRef('Hello, World!'); // Replace with your desired text
-
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     if (isWriting) {
-  //       setText((prevText) => prevText + textRef.current[prevText.length]);
-  //       if (prevText.length === textRef.current.length - 1) {
-  //         setIsWriting(false);
-  //       }
-  //     } else {
-  //       setText((prevText) => prevText.slice(0, -1));
-  //       if (prevText.length === 0) {
-  //         setIsWriting(true);
-  //       }
-  //     }
-  //   }, 100); // Adjust the interval time as needed
-
-  //   return () => clearInterval(intervalId);
-  // }, []);
-
-
-  const array = ['Machine Learning Developer', 'Web Developer', 'Python Developer'];
+  const array = ['ML Developer', 'Web Developer', 'Python Developer'];
   var index = 0;
   var textIndex = 0;
-  const [ text, setText ] = useState(array[index][textIndex]);
+  var word = '';
+  var lengthFoWord = 0;
+  const [text, setText] = useState(array[index][textIndex]);
 
   useEffect(() => {
-    setInterval(() => {
-      const word = array[index];
-      if ( index < 2 ) {
-        index++;
-        setInterval(() => {
-          if ( textIndex < word.length - 1 ) {
+    const intervalID = setInterval(() => {
+      word = array[index];
+      lengthFoWord = word.length;
+      if (index < 3) {
+        const interval = setInterval(() => {
+          if (textIndex < lengthFoWord - 1) {
             textIndex++;
-            setText((prevText) => { return prevText + word[textIndex] })
+            setText((prevText) => { return prevText + word[textIndex] });
+          }
+          else {
+            clearInterval(interval);
           }
         }, 100);
+        index++;
         textIndex = 0;
-        setText(word[textIndex]);
+        setText(word[textIndex]); 
       }
-      else {
+      if (index === 3) {
         index = 0;
       }
-    }, 2000);
+    }, 3000);
+
+    return () => clearInterval(intervalID); 
   }, []);
+
+  const [ header, setHeader ] = useState('');
+
+  useEffect(() => {
+    const heading = "Heello! I am Prashik";
+    const headingLength = heading.length;
+    var headingIndex = 0;
+    const headingInterval = setInterval(() => {
+      if (headingIndex < headingLength - 1) {
+        setHeader((prevHeader) => { return prevHeader + heading[headingIndex] });
+        headingIndex++;
+      }
+      else {
+        clearInterval(headingInterval);
+      }
+    }, 100);
+
+    return () => clearInterval(headingInterval);
+  }, []); 
 
   return (
     <div id='home'>
-        <img alt='profile_pic' src={profile_pic} id='profile_pic'/>
-        <div id="home_circle"></div>
-        <img alt='grid' src={grid} id='grid' />
-        <div id="home_header">
-          <h1>Hello! I am Prashik</h1>
-          <p>{text}</p>
-        </div>
-        <div id="social_links">
-          <a href='https://www.github.com/PrashikBhimte/' target='blank' ><img src={github} alt="github" /></a>
-          <a href='https://www.instagram.com/prashikbhimte29/' target='blank'><img src={instagram} alt='instagram' /></a>
-          <a href='https://www.linkedin.com/in/prashikbhimte29/' target='blank'><img src={linkedin} alt="linkedin" /></a>
-        </div>
+      <img alt='profile_pic' src={profile_pic} id='profile_pic' />
+      <div id="home_circle"></div>
+      <img alt='grid' src={grid} id='grid' />
+      <div id="home_header">
+        <h1>{header}</h1>
+        <p>{text}</p>
+      </div>
+      <div id="social_links">
+        <a href='https://www.github.com/PrashikBhimte/' target='blank' ><img src={github} alt="github" /></a>
+        <a href='https://www.instagram.com/prashikbhimte29/' target='blank'><img src={instagram} alt='instagram' /></a>
+        <a href='https://www.linkedin.com/in/prashikbhimte29/' target='blank'><img src={linkedin} alt="linkedin" /></a>
+      </div>
     </div>
   )
 }
