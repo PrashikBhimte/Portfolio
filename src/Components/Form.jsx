86fbd47d496f62fba07b681ffa5e26401055448b
PrashikBhimte <prashikbhimte29@gmail.com>
2025-03-26 20:20:05 +0530

    portfolio server is been updated. now all the messages are sent through the email

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,31 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./Form.css";
 
 export default function Form() {
 
-  const [ name, setName ] = useState('');
-  const [ email, setEmail ] = useState('');
-  const [ subject, setSubject ] = useState('');
-  const [ message, setMessage ] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleChangeName = (event) => {
     setName(event.target.value);
-  }
+  };
 
   const handleChangeEmail = (event) => {
     setEmail(event.target.value);
-  }
+  };
 
   const handleChangeSubject = (event) => {
     setSubject(event.target.value);
-  }
+  };
 
   const handleChangeMessage = (event) => {
     setMessage(event.target.value);
-  }
+  };
 
   const handleSubmit = async () => {
-    await fetch("https://prashikbhimte29.pythonanywhere.com/sendmessage", {
+    const responce = await fetch("https://portfolio-server-hoys.onrender.com/sendmessage", {
       method : "POST",
       headers : {
         'Content-Type': 'application/json'
@@ -37,22 +37,46 @@ export default function Form() {
         "message" : message
       })
     });
-    alert("Message has been send!");
-    setName("");
-    setEmail("");
-    setSubject("");
-    setMessage("");
-  }
+    if (!responce.ok) {
+      alert("Error in sending message!");
+    }
+    else {
+      alert("Message has been send!");
+      setName("");
+      setEmail("");
+      setSubject("");
+      setMessage("");
+    }
+  };
 
   return (
-    <div id='form'>
-        <div>
-            <input type='text' placeholder='Your Name' value={name} onChange={handleChangeName} />
-            <input type='text' placeholder='Your Email' value={email} onChange={handleChangeEmail} />
-        </div>
-        <input type='text' placeholder='Subject' value={subject} onChange={handleChangeSubject} />
-        <textarea placeholder='Message' value={message} onChange={handleChangeMessage} />
-        <button onClick={handleSubmit}>Send Message</button>
+    <div id="form">
+      <div>
+        <input
+          type="text"
+          placeholder="Your Name"
+          value={name}
+          onChange={handleChangeName}
+        />
+        <input
+          type="text"
+          placeholder="Your Email"
+          value={email}
+          onChange={handleChangeEmail}
+        />
+      </div>
+      <input
+        type="text"
+        placeholder="Subject"
+        value={subject}
+        onChange={handleChangeSubject}
+      />
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={handleChangeMessage}
+      />
+      <button onClick={handleSubmit}>Send Message</button>
     </div>
-  )
+  );
 }
