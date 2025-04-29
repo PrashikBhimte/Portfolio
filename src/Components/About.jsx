d66da89ec011c159b7b4ba7f93196b604b724128
Prashik Gajanan Bhimte <prashikbhimte29@gmail.com>
2025-04-29 12:25:49 +0530

    Update About.jsx, resume is shifted from the local storage to the google drive link.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -11,7 +11,7 @@ export default function About() {
         <h1>About Me</h1>
         <p>Hi there! I'm Prashik Bhimte, a machine learning enthusiast with a passion for building innovative AI applications. With a strong foundation in Python, I've developed a solid understanding of machine learning concepts and have completed several projects in the field.
         </p>
-        <a id='about_but' href={resume} target='blank'>Download CV</a>
+        <a id='about_but' href="https://drive.google.com/uc?export=view&id=1NRoByFcMU4w3IONGgdmw0wnsnrNghSY0" target='blank'>Download CV</a>
       </div>
       <div id="about_pic">
         <div id="about_pic_img"></div>
